2c6ddd786067463396b437e7ce59bb8dc6d2fc8f
Erick Rodrigues <149729630+erickki@users.noreply.github.com>
2025-08-28 23:44:35 -0300

    Update
    28/08/2025 - 23:44

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request
 
-from src.login import validar_login
-from src.registro import validar_registro
-from src.esqueceu_senha import validar_esqueceu_senha
+from src.sistema_login import fazer_login
+from src.sistema_login import fazer_esqueci_senha
+from src.sistema_login import fazer_resetar_senha
+from src.sistema_login import fazer_registro
 
 app = Flask(__name__)
 
@@ -14,68 +15,21 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/fazer_login', methods=['POST'])
-def fazer_login():
-    entrada_email = request.form.get('entrada_email_validar')
-    entrada_senha = request.form.get('entrada_senha_validar')
-    verificacao_login = validar_login(entrada_email, entrada_senha)
+@app.route('/esqueci_senha')
+def esqueci_senha():
+    return render_template('esqueci_senha.html')
 
-    if verificacao_login == 'pode logar':
-        return redirect(url_for('registro'))
-    elif verificacao_login == 'sem cadastro':
-        return render_template('login.html', erro='Usuário não cadastrado.')
-    elif verificacao_login == 'senha errada':
-        return render_template('login.html', erro='Senha incorreta.')
-    else:
-        return render_template('login.html', erro='Erro desconhecido.')
+@app.route('/resetar_senha')
+def resetar_senha():
+    return render_template('resetar_senha.html')
 
 @app.route('/registro')
 def registro():
     return render_template('registro.html')
 
-@app.route('/fazer_registro', methods=['POST'])
-def fazer_registro():
-    entrada_nome = request.form.get('entrada_nome_validar')
-    entrada_email = request.form.get('entrada_email_validar')
-    entrada_senha = request.form.get('entrada_senha_validar')
-    entrada_senha2 = request.form.get('entrada_senha2_validar')
-    verificacao_registro = validar_registro(entrada_nome, entrada_email, entrada_senha, entrada_senha2)
-
-    if verificacao_registro == 'registrado':
-        return redirect(url_for('login'))
-    elif verificacao_registro == 'falta informacao':
-        return render_template('registro.html', erro='Preencha todos os campos.')
-    elif verificacao_registro == 'ja registrado':
-        return render_template('registro.html', erro='Email já registrado.')
-    elif verificacao_registro == 'senhas diferentes':
-        return render_template('registro.html', erro='As senhas precisam ser iguais.')
-    else:
-        return render_template('login.html', erro='Erro desconhecido.')
-
-@app.route('/esqueceu_senha')
-def esqueceu_senha():
-    return render_template('esqueceu_senha.html')
-
-@app.route('/fazer_esqueceu_senha', methods=['POST'])
-def fazer_esqueceu_senha():
-    entrada_nome = request.form.get('entrada_nome_validar')
-    entrada_email = request.form.get('entrada_email_validar')
-    verificacao_esqueceu_senha = validar_esqueceu_senha(entrada_nome, entrada_email)
-    if verificacao_esqueceu_senha == 'codigo gerado':
-        return redirect(url_for('esqueceu_senha2'))
-    elif verificacao_esqueceu_senha == 'falta informacao':
-        return render_template('esqueceu_senha.html', erro='Preencha todos os campos.')
-    elif verificacao_esqueceu_senha == 'nome incorreto':
-        return render_template('esqueceu_senha.html', erro='Nome incorreto.')
-    elif verificacao_esqueceu_senha == 'email nao registrado':
-        return render_template('esqueceu_senha.html', erro='Email não registrado.')
-    else:
-        return render_template('esqueceu_senha.html', erro='Erro desconhecido.')
-
-    
-@app.route('/resetar_senha')
-def resetar_senha():
-    return render_template('resetar_senha.html')
+@app.route('/sistema_inicio')
+def sistema_inicio():
+    return render_template('sistema_inicio.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/sistema_login.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+banco_dados = 'data/banco_dados_roxho.db'
+
+def fazer_login():
+    pass
+
+def fazer_esqueci_senha():
+    pass
+
+def fazer_resetar_senha():
+    pass
+
+def fazer_registro():
+    pass
+
